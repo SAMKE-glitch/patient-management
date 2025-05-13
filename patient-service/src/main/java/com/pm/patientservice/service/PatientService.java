597057421470ac3f88dc465c9263fe09a3e36b54
samke-glitch <sammythemwa@gmail.com>
2025-05-13 12:41:24 +0300

    mapper package

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -1,5 +1,6 @@
 package com.pm.patientservice.service;
 
+import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
 import org.springframework.stereotype.Service;
