a1f04ad56e9cf259222680e77fd2df935e29e58d
samke-glitch <sammythemwa@gmail.com>
2025-05-13 12:24:32 +0300

    pbjects

--- patient-service/src/main/java/com/pm/patientservice/dto/PatientResponseDTO.java
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientResponseDTO.java
@@ -1,4 +1,9 @@
 package com.pm.patientservice.dto;
 
 public class PatientResponseDTO {
+    private String Id;
+    private String Name;
+    private String email;
+    private String address;
+    private String dateOfBirth;
 }
