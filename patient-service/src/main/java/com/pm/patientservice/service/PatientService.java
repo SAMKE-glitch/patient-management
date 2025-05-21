478e98d54ad27eb87adfc8104b9b6c5044aec4e0
samke-glitch <sammythemwa@gmail.com>
2025-05-21 12:34:42 +0300

    create patient object

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -1,5 +1,6 @@
 package com.pm.patientservice.service;
 
+import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
@@ -18,10 +19,16 @@ public class PatientService {
         this.patientRepository = patientRepository;
     }
 
+
     public List<PatientResponseDTO> getPatients() {
         List<Patient> patients = patientRepository.findAll();
 
         return patients.stream()
                 .map(PatientMapper::toDTO).toList();
+    }
+
+
+    public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
+
     }
 }
