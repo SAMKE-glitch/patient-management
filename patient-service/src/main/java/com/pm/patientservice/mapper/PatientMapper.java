e15896e1cb3946f7572b4dff985a3d9ecb42ce24
samke-glitch <sammythemwa@gmail.com>
2025-05-22 11:48:46 +0300

    patient model entity generated

--- patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -4,6 +4,8 @@ import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.model.Patient;
 
+import java.time.LocalDate;
+
 public class PatientMapper {
     public static PatientResponseDTO toDTO(Patient patient) {
         PatientResponseDTO patientDTO = new PatientResponseDTO();
@@ -16,6 +18,13 @@ public class PatientMapper {
     }
 
     public static Patient toModel(PatientRequestDTO patientRequestDTO) {
+        Patient patient = new Patient();
 
+        patient.setName(patientRequestDTO.getName());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+        return patient;
     }
 }
