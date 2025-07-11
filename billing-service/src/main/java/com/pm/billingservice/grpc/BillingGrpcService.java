1e9f180931df46f17ec1b25d42ee750784ef1648
samke-glitch <sammythemwa@gmail.com>
2025-07-11 12:58:55 +0300

    marked grpc-java folder as generated source root and impl BillingGrpcService

--- billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -1,6 +1,8 @@
 package com.pm.billingservice.grpc;
 
 
+import billing.BillingRequest;
+import billing.BillingResponse;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import billing.BillingServiceGrpc.BillingServiceImplBase;
@@ -8,7 +10,7 @@ import billing.BillingServiceGrpc.BillingServiceImplBase;
 @GrpcService
 public class BillingGrpcService extends BillingServiceImplBase {
     @Override
-    public void createBillingAcount(billing.BillingRequest billingRequest, StreamObserver<billing.BillingResponse> responseStreamObserver){
+    public void createBillingAcount(BillingRequest billingRequest, StreamObserver<BillingResponse> responseStreamObserver){
 
     }
 }
