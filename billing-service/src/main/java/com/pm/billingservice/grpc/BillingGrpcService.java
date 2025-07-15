d410693e5d63ae0331460e5df91447def3ec0fbc
samke-glitch <sammythemwa@gmail.com>
2025-07-15 12:11:50 +0300

    responseObserver completed

--- billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -25,5 +25,6 @@ public class BillingGrpcService extends BillingServiceImplBase {
                 .build();
 
         responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
 }
