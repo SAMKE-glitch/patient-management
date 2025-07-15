577c85856b0bf5cfa1707a5d7386c42de5f580cd
samke-glitch <sammythemwa@gmail.com>
2025-07-15 11:52:59 +0300

    adding logger slf4j

--- billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -6,11 +6,16 @@ import billing.BillingResponse;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import billing.BillingServiceGrpc.BillingServiceImplBase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @GrpcService
 public class BillingGrpcService extends BillingServiceImplBase {
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
     @Override
     public void createBillingAcount(BillingRequest billingRequest, StreamObserver<BillingResponse> responseObserver){
+        log.info("CreateBillignAccount request received {}", billingRequest.toString());
 
     }
 }
