bb4ca0fbd016e9e2386d6a3c4888ceaf97401081
samke-glitch <sammythemwa@gmail.com>
2025-07-18 12:58:18 +0300

    creating billingservicegrpcclient

--- patient-service/src/main/java/com/pm/patientservice/grpc/BillingServiceGrpcClient.java
+++ patient-service/src/main/java/com/pm/patientservice/grpc/BillingServiceGrpcClient.java
@@ -1,4 +1,14 @@
 package com.pm.patientservice.grpc;
 
+
+import billing.BillingServiceGrpc;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BillingServiceGrpcClient {
+    private final BillingServiceGrpc.BillingServiceBlockingStub blockingStub;
+
+    public BillingServiceGrpcClient(@Value("${billing.service.address:localhost}") String serverAdress, @Value("${billing.service.grpc.port:9001}") int serverPort) {
+    }
 }
