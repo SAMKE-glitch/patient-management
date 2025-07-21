06c3aa2ab482d46368a0163714ae5e27d4b35e85
samke-glitch <sammythemwa@gmail.com>
2025-07-21 12:40:52 +0300

    logging to help with debugging

--- patient-service/src/main/java/com/pm/patientservice/grpc/BillingServiceGrpcClient.java
+++ patient-service/src/main/java/com/pm/patientservice/grpc/BillingServiceGrpcClient.java
@@ -2,13 +2,17 @@ package com.pm.patientservice.grpc;
 
 
 import billing.BillingServiceGrpc;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BillingServiceGrpcClient {
+    private static final Logger log = LoggerFactory.getLogger(BillingServiceGrpcClient.class);
     private final BillingServiceGrpc.BillingServiceBlockingStub blockingStub;
 
     public BillingServiceGrpcClient(@Value("${billing.service.address:localhost}") String serverAdress, @Value("${billing.service.grpc.port:9001}") int serverPort) {
+        log.info("Connecting to Billing Service GRPC Service at: {}:{}", serverAdress, serverPort);
     }
 }
