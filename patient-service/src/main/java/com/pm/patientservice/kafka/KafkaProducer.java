d83207c0b105ebf23710d4d7da371ac4fbbeed70
samke-glitch <sammythemwa@gmail.com>
2025-07-30 12:38:20 +0300

    kafka template added

--- patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
+++ patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
@@ -1,4 +1,8 @@
 package com.pm.patientservice.kafka;
 
+import org.springframework.kafka.core.KafkaTemplate;
+
 public class KafkaProducer {
+
+    private final KafkaTemplate<String, byte[]> kafkaTemplate;
 }
