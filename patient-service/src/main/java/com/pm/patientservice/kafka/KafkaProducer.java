99e3c289c70fafffc4614a4e29f18f336dafc06a
samke-glitch <sammythemwa@gmail.com>
2025-07-30 12:47:47 +0300

    kafkaTemplate initialized

--- patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
+++ patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
@@ -5,4 +5,8 @@ import org.springframework.kafka.core.KafkaTemplate;
 public class KafkaProducer {
 
     private final KafkaTemplate<String, byte[]> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
 }
