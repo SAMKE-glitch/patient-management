ec11c70c85bb16b5c278f80b2bdfb89269b5133b
samke-glitch <sammythemwa@gmail.com>
2025-08-01 12:40:09 +0300

    patient event created based on the patient object from patientservice.model package

--- patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
+++ patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
@@ -3,6 +3,7 @@ package com.pm.patientservice.kafka;
 import com.pm.patientservice.model.Patient;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
 
 @Service
 public class KafkaProducer {
@@ -14,6 +15,11 @@ public class KafkaProducer {
     }
 
     public void sendEvent(Patient patient) {
-
+        PatientEvent event = PatientEvent.newBuilder()
+                .setPatientId(patient.getId().toString())
+                .setName(patient.getName())
+                .setEmail(patient.getEmail())
+                .setEventType("PATIENT_CREATED")
+                .build();
     }
 }
