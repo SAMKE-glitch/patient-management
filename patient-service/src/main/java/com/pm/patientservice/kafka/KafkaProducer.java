25fef3088b54127090e359ce096e417608a4cb40
samke-glitch <sammythemwa@gmail.com>
2025-08-11 12:07:35 +0300

    try catch block for sending event

--- patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
+++ patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
@@ -1,6 +1,8 @@
 package com.pm.patientservice.kafka;
 
 import com.pm.patientservice.model.Patient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import patient.events.PatientEvent;
@@ -8,6 +10,7 @@ import patient.events.PatientEvent;
 @Service
 public class KafkaProducer {
 
+    private static final Logger log = LoggerFactory.getLogger(KafkaProducer.class);
     private final KafkaTemplate<String, byte[]> kafkaTemplate;
 
     public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
@@ -21,5 +24,11 @@ public class KafkaProducer {
                 .setEmail(patient.getEmail())
                 .setEventType("PATIENT_CREATED")
                 .build();
+
+        try {
+
+        } catch (Exception e) {
+            log.error("Error sending PatientCreated event: {}", event);
+        }
     }
 }
