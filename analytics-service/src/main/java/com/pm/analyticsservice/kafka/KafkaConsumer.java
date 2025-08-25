fc33a46bec94ec17f21998810515ad03a4666996
samke-glitch <sammythemwa@gmail.com>
2025-08-25 12:55:33 +0300

    kafka consumer method

--- analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
+++ analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
@@ -2,6 +2,8 @@ package com.pm.analyticsservice.kafka;
 
 
 import com.google.protobuf.InvalidProtocolBufferException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import patient.events.PatientEvent;
@@ -9,13 +11,15 @@ import patient.events.PatientEvent;
 @Service
 public class KafkaConsumer {
 
+    private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+
     @KafkaListener(topics = "patient", groupId = "analytics-service")
     public void consumeEvent(byte[] event){
 
         try {
             PatientEvent patientEvent = PatientEvent.parseFrom(event);
         } catch (InvalidProtocolBufferException e) {
-            throw new RuntimeException(e);
+            log.error("Error deserializing event {}", e.getMessage());
         }
 
     }
