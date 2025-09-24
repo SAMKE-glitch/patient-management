56f3bc3de3ad60e600072959addb29f3760e3d73
samke-glitch <sammythemwa@gmail.com>
2025-09-24 12:26:06 +0300

    AuthService class authenticate method

--- auth-service/src/main/java/com/pm/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -1,4 +1,14 @@
 package com.pm.authservice.service;
 
+import com.pm.authservice.dto.LoginRequestDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class AuthService {
+
+    public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
+
+    }
 }
