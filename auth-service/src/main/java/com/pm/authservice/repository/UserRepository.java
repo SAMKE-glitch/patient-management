94b9a56fdffa02f6ca74c23a747cb4bea5d62581
samke-glitch <sammythemwa@gmail.com>
2025-09-25 12:50:13 +0300

    user repository interface

--- auth-service/src/main/java/com/pm/authservice/repository/UserRepository.java
+++ auth-service/src/main/java/com/pm/authservice/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.pm.authservice.repository;
 
-public interface UserRepository {
+import com.pm.authservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
 }
