15c20a869550ab2b065a718974b8277d1f3e5b57
samke-glitch <sammythemwa@gmail.com>
2025-10-06 15:54:50 +0300

    security config plus package chain method

--- auth-service/src/main/java/com/pm/authservice/config/SecurityConfig.java
+++ auth-service/src/main/java/com/pm/authservice/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.pm.authservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,6 +11,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
 
+        return http.build();
     }
 }
