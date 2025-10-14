f5946a5b985e1bf5652b95ce484fa5b6a112346e
samke-glitch <sammythemwa@gmail.com>
2025-10-14 16:33:32 +0300

    jwt util class

--- auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
+++ auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
@@ -1,12 +1,16 @@
 package com.pm.authservice.util;
 
+import org.springframework.beans.factory.annotation.Value;
+
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.util.Base64;
 
 public class JwtUtil {
 
     private final Key secretKey;
 
-    public JwtUtil(Key secretKey) {
-        this.secretKey = secretKey;
+    public JwtUtil(@Value("${jwt.secret}") String secret){
+        byte[] keyBytes = Base64.getDecoder().decode(secret.getBytes(StandardCharsets.UTF_8));
     }
 }
